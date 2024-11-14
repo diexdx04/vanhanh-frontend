4029b0a0b1e3fb5f17df73684de2995dc69e62cf
diexdx04 <diepdx.dev@gmail.com>
2024-11-14 15:31:04 +0700

    login

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,128 @@
+"use client";
+import { instance } from "@/api/instance";
+import { Button, Form, FormProps, Input, message } from "antd";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+type FieldType = {
+  password?: string;
+  email: string;
+};
+
+const Page: React.FC = () => {
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const [formData, setFormData] = useState<FieldType>({
+    password: "",
+    email: "",
+  });
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+    console.log("values:", values);
+    setFormData(values);
+    setIsSubmit(true);
+  };
+
+  const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (
+    errorInfo
+  ) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  useEffect(() => {
+    const submitForm = async () => {
+      try {
+        const response = await instance.post("/user/signin", formData);
+        console.log(response, 3333);
+        messageApi.success("dang nhap thanh cong!");
+
+        console.log("day la token:", response.data.access_token.access_token);
+
+        localStorage.setItem("token", response.data.access_token.access_token);
+      } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+          let errorMessage = "";
+          if (error.response) {
+            if (
+              error.response.data &&
+              typeof error.response.data === "object"
+            ) {
+              errorMessage = error.response.data.message || "da xay ra loi!";
+            } else {
+              errorMessage = error.response.data;
+            }
+          } else {
+            errorMessage = error.message;
+          }
+          console.error("loi:", errorMessage);
+          messageApi.error(errorMessage);
+        } else {
+          console.error("loi khong xac dinh:", error);
+          messageApi.error("Dang nhap that bai");
+        }
+      } finally {
+        setIsSubmit(false);
+      }
+    };
+
+    if (isSubmit) {
+      submitForm();
+    }
+  }, [isSubmit, formData]);
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-200">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        {contextHolder}
+        <h1 className="text-2xl font-bold mb-6">Dang nhap</h1>
+        <Form
+          name="basic"
+          layout="vertical"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              { required: true, message: "vui long khong de trong" },
+              {
+                type: "email",
+                message: "chua dung dinh dang email",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              { required: true, message: "vui long khong de trong" },
+              {
+                min: 6,
+                message: "mat khau phai it nhat 6 ky tu",
+              },
+              {
+                max: 20,
+                message: "mat khau khong duoc dai qua 20 ky tu",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="w-full">
+              Dang Nhap
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
