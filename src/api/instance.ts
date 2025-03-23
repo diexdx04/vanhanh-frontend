9314a8ddfc69614ad3fc92786c579c0a370d9a8c
diexdx04 <diepdx.dev@gmail.com>
2025-03-23 17:07:11 +0700

    update port

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 import { io } from "socket.io-client";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://vanhanh-backend-production.up.railway.app/",
+
   timeout: 10000,
 });
 
